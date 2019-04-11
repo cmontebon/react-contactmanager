cccc0974d5d86ae3064712094fb9b03034d8bbd9
Christian Montebon <cpmontebon25@gmail.com>
2019-04-11 17:43:48 +0800

    Add context api

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,57 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import { Consumer } from "../context";
+
 class Contact extends Component {
   state = {
     showContactInfo: false
   };
 
-  onShowClick = e => {
+  onShowClick = () => {
     this.setState({ showContactInfo: !this.state.showContactInfo });
   };
 
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
+  };
+
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
     const icon = showContactInfo
       ? "fas fa-times-circle"
       : "fas fa-chevron-circle-down";
 
     return (
-      <div className="card card-body mb-3">
-        <h3>
-          {name}
-          <i className={icon} onClick={this.onShowClick} />
-        </h3>
-        {showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        ) : null}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h3>
+                {name}{" "}
+                <i
+                  className={icon}
+                  onClick={this.onShowClick}
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  className="fas fa-times"
+                  style={{ color: "red", cursor: "pointer", float: "right" }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+              </h3>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,39 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 export class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "Chris Smith",
-        email: "cs@gmail.com",
-        phone: "555 5555-55555"
-      },
-      {
-        id: 2,
-        name: "John Doe",
-        email: "jd@gmail.com",
-        phone: "666 6666-66666"
-      },
-      {
-        id: 3,
-        name: "Foo Bar",
-        email: "fb@gmail.com",
-        phone: "777 7777-77777"
-      }
-    ]
-  };
-
   render() {
-    const { contacts } = this.state;
-
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
