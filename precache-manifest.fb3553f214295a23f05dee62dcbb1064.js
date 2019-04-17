4c0ed726e323c4120232d3961c8215fead61a3f5
Christian Montebon <cpmontebon25@gmail.com>
2019-04-17 13:40:50 +0800

    Updates

---
+++ precache-manifest.fb3553f214295a23f05dee62dcbb1064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "817ea11d46679383a3d6",
+    "url": "/react-contactmanager/static/js/runtime~main.ac9e1aa1.js"
+  },
+  {
+    "revision": "d78eadbe356022857fb3",
+    "url": "/react-contactmanager/static/js/main.5246da5d.chunk.js"
+  },
+  {
+    "revision": "5fd2a7fb5e55760c7108",
+    "url": "/react-contactmanager/static/js/2.e07a7efb.chunk.js"
+  },
+  {
+    "revision": "d78eadbe356022857fb3",
+    "url": "/react-contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "5fd2a7fb5e55760c7108",
+    "url": "/react-contactmanager/static/css/2.0bc9cb66.chunk.css"
+  },
+  {
+    "revision": "c37aa30d16e3840eae1805cc1ee269bd",
+    "url": "/react-contactmanager/index.html"
+  }
+];
